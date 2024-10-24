de18aedee79b625af937a2b2d6b261b1057b770e
orangedahlia <dorrc715@newschool.edu>
2024-10-23 20:43:15 -0400

    hamburger menu fix
    hamburger menu fix

---
+++ tryingagain/hamburgermenu/menu.js
@@ -0,0 +1,9 @@
+function showmenu() {
+	document.getElementById('menuoverlay').style.display="block";
+	document.getElementById('close').style.display="inline-block";
+}
+
+function hidemenu(){
+	document.getElementById('menuoverlay').style.display="none";
+	document.getElementById('close').style.display="none";
+}
